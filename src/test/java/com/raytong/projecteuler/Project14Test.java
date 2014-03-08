c97e43ab5aeaaf7be8f2ec5f5c3738b4de7a04cd
Raymond Tong <raytong82@gmail.com>
2014-03-08 12:38:24 +0800

    initial commit

---
+++ src/test/java/com/raytong/projecteuler/Project14Test.java
@@ -0,0 +1,13 @@
+package com.raytong.projecteuler;
+
+import org.junit.Test;
+
+/**
+ * Created by raytong on 3/8/14.
+ */
+public class Project14Test {
+    @Test
+    public void shouldPass() {
+
+    }
+}
